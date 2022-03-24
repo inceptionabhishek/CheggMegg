915d6791388daf77aba1995ad9fef8cf968d2aa5
Abhishek kumar <abhishek1122338@gmail.com>
2022-03-24 10:50:17 +0530

    Added Spinner

--- client/src/Pages/Explore.js
+++ client/src/Pages/Explore.js
@@ -2,15 +2,18 @@ import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { Grid, Typography, Box } from "@mui/material";
+import { Spinner } from "react-bootstrap";
 import CardComponentForExplore from "../Components/CardComponentForExplore";
 function Explore() {
   const api = "https://meggchegg.herokuapp.com/api/solved/view/all";
   const [allQuestions, setAllQuestions] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     axios
       .get(api)
       .then((res) => {
         setAllQuestions(res.data.data);
+        setLoading(false);
         console.log(res);
       })
       .catch((err) => {
@@ -21,20 +24,35 @@ function Explore() {
   return (
     <>
       <h2 className="Top-Section2">Explore All Questions</h2>
-      <div className="container">
-        <Grid
-          container
-          spacing={3}
-          direction="row"
-          justify="center"
-          alignItems="center"
-          padding="2px"
-        >
-          {allQuestions.map((question) => {
-            return <CardComponentForExplore data={question} />;
-          })}
-        </Grid>
-      </div>
+      {loading ? (
+        <>
+          {" "}
+          <div className="spinner">
+            <Spinner
+              animation="border"
+              variant="primary"
+              className="TempSpinner"
+            />
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="container">
+            <Grid
+              container
+              spacing={3}
+              direction="row"
+              justify="center"
+              alignItems="center"
+              padding="2px"
+            >
+              {allQuestions.map((question) => {
+                return <CardComponentForExplore data={question} />;
+              })}
+            </Grid>
+          </div>
+        </>
+      )}
     </>
   );
 }
