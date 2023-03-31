42beb773d562d2ca0c725d421ba3de14e7e73988
Abhishek <abhishek1122338@gmail.com>
2023-03-31 23:04:14 +0530

    mongo db error

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ const openai = new OpenAIApi(configuration);
 
 const PORT = process.env.PORT || 5000;
 const uri = process.env.MONGO_URI;
-mongoose.connect(uri, { useNewUrlParser: true });
+mongoose.connect(uri, {
+  useNewUrlParser: "true",
+  useUnifiedTopology: "true",
+});
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
