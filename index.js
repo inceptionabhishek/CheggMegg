1528ae411f295dd158e5319eb2af41c2b751ee00
Abhishek <abhishek1122338@gmail.com>
2023-03-31 12:54:56 +0530

    🧯 f

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 
-require("dotenv").config({ path: ".env" });
+require("dotenv").config();
 const app = express();
 
 app.use(express.json());
